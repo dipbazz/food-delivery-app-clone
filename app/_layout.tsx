7538df4446088353aaaeae61a44b2ae6300638b7
Dipesh Bajgain <dipbazz@gmail.com>
2024-06-11 17:13:53 +0545

    Add appbar with custom layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,36 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
+import CustomHeader from '@/components/CustomHeader';
+import { DefaultTheme, PaperProvider } from 'react-native-paper';
 
-import { useColorScheme } from '@/components/useColorScheme';
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: 'index',
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+const theme = {
+  ...DefaultTheme,
+  // Specify custom property
+  myOwnProperty: true,
+  // Specify custom property in nested object
+  colors: {
+    ...DefaultTheme.colors,
+  },
+};
 
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+export default function RootLayoutNav() {
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <PaperProvider theme={theme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen
+          name="index"
+          options={{
+            header: () => <CustomHeader />
+          }}
+        />
       </Stack>
-    </ThemeProvider>
+    </PaperProvider>
   );
 }
---
+++ components/CustomHeader.tsx
@@ -0,0 +1,71 @@
+import { View, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Appbar, Avatar, Text, TextInput, useTheme } from 'react-native-paper'
+import { Ionicons } from '@expo/vector-icons';
+
+
+const CustomHeader = () => {
+  const theme = useTheme();
+  const _handleProfile = () => console.log("profile clicked.")
+
+  return (
+    <SafeAreaView>
+      <Appbar.Header style={{backgroundColor: theme.colors.background }}>
+          <Appbar.Content
+            title={
+              <View>
+                <TouchableOpacity style={styles.heading}>
+                  <Avatar.Text size={35} label="FD" style={{ marginRight: 5 }} />
+                  <View>
+                    <Text style={{ color: theme.colors.secondary }}>Delivery name</Text>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                      <Text variant='labelLarge' style={{ fontWeight: 'bold' }}>London</Text>
+                      <Ionicons name="chevron-down" size={15} color={theme.colors.primary} style={{ marginTop: 5}}/>
+                    </View>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            }
+          />
+          <Appbar.Action icon="account-outline" onPress={_handleProfile} color={theme.colors.primary}/>
+      </Appbar.Header>
+      <View
+        style={{
+          marginLeft: 15,
+          marginRight: 15,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          paddingTop: 5,
+          paddingBottom: 10
+        }}
+      >
+        <TextInput
+          mode='outlined'
+          style={{ width: '90%', height: 40 }}
+          contentStyle= {{ margin: 0 }}
+          outlineStyle={{ borderWidth: 0, backgroundColor: theme.colors.surface }}
+          // value={text}
+          // onChangeText={text => setText(text)}
+        />
+        <TouchableOpacity>
+          <Ionicons name='options-outline' size={20} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  header: {
+    height: 50,
+  },
+  heading: {
+    display: 'flex',
+    flexDirection: "row"
+  }
+});
+
+export default CustomHeader
